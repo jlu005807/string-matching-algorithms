4f5dfa71151e808089416323decbec085d89ae75
jlu005807 <1289406660@qq.com>
2024-09-29 23:51:57 +0800

    nearly understand suffixes

--- BM.h
+++ BM.h
@@ -16,7 +16,8 @@
 
 class BM :public Index
 {
-	
+
+public:
 	//生成坏字符偏移表
 	void preBmBc(std::string T, int Bmbc[])
 	{
@@ -36,9 +37,10 @@ class BM :public Index
 
 	}
 
-	//生成suffix表辅助生成好后缀偏移表
+	//生成suffix表辅助接下来生成好后缀偏移表
 	//suffix[ i ] = shift 表示以 i 为右边界，与模式串T后缀匹配的最大长度，
 	//亦即T[i - shift + 1 , i] = T[len - shift , len - 1]
+	//此法较难理解，有简单实现
 	void suffixes(std::string T, int suffix[])
 	{
 		int lenT = T.size();
@@ -47,16 +49,21 @@ class BM :public Index
 
 		int g = lenT - 1;//表示当前右边界
 
-		int f=lenT-2;//当前后缀起始位置
+		int f=lenT-2;//当前后缀起始位置(从左往右）
 
 		for (int i = lenT - 2; i >= 0; i--)//从最后第二个字符往前回溯
 		{
 			if (i > g && suffix[i + lenT - 1 - f] < i - g)
 			{
-				suffix[i] = suffix[i + lenT - 1 - f];
+				//当i>g时，T[g+1,f]已经是匹配了T[lenT-1-f+g+1,lenT-1]
+				//
+				/*std::cout << i << " " << g << " " << f << " ";
+				system("Pause");*/
+				suffix[i] = suffix[i + lenT - 1 - f];//继承之前计算的值
 			}
 			else
 			{
+
 				if (i < g)g = i;//如果回溯到边界左边，则更新右边界
 
 				f = i;//更新当前后缀的起始位置
@@ -69,10 +76,28 @@ class BM :public Index
 		}
 	}
 
+	//简单生成suffix辅助表
+	void simply_suffixes(std::string T, int suffix[])
+	{
+		int lenT = T.size();
+		
+		//同理，最后一个字符的匹配长度就是T本身长度
+		suffix[lenT - 1] = lenT;
+
+		//两次循环
+		for (int i = lenT - 2; i >= 0; i--)
+		{
+			int j = 0;//记录已经匹配长度
+			for (j; j <= i && T[i - j] == T[lenT - j - 1]; j++) {}
+			suffix[i] = j;
+		}
+
+	}
+
 	
 	int index(std::string S, std::string T, int pos)
 	{
-
+		return 0;
 	}
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include"BF.h"
 #include"KMP.h"
+#include"BM.h"
 
 
 
@@ -7,6 +8,53 @@
 
 int main()
 {
+
+	/*std::string e("abcabcdefjkbc");
+
+	const int i = e.size();
+	int a[13], b[13];
+
+	BM bm;
+
+	clock_t n = clock();
+
+	
+	bm.suffixes("BABAAB", a);
+
+	clock_t p = clock();
+
+	int d1=p-n;
+
+	n = clock();
+
+	for (int i = 0; i < 50000; i++)
+	bm.simply_suffixes(e, b);
+
+	p = clock();
+
+	int d2 = p - n;
+
+	for (int i = 0; i < 13; i++)
+	{
+		std::cout << a[i] << " ";
+	}
+
+	std::cout <<d1<< std::endl;
+	for (int i = 0; i < 13; i++)
+	{
+		std::cout << b[i] << " ";
+	}
+	std::cout << d2 << std::endl;*/
+
+	int a[12];
+	BM bm;
+	bm.suffixes("BABAAB", a);
+
+	for (int i = 0; i < 13; i++)
+	{
+		std::cout << a[i] << " ";
+	}
+
 	std::string S("maba66o");
 
 	std::string T("aba");
